8087c4bb8d7a4edcff459eb407fe313136ed045f
ciittraininginstitute <ciitinstitutepune@gmail.com>
2026-06-17 10:42:55 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Welcome to React Js Application
+        </p>
+        <a
+          className="App-link"
+          href="https://ciitinstitute.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CIIT Training Insitute
+        </a>
+        <h4>Hi Hello</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
